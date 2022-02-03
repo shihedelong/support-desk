fe19db85b71e3259baf01d86ee071e05a3d7dae9
shihedelong <shihedelong@gmail.com>
2022-02-03 21:18:03 +1100

    Home, Login & Register UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>What do you need help with? </h1>
+      </section>
+
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Tickect
+      </Link>
+
+      <Link to='/ticket' className='btn  btn-block'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
   )
 }
 
