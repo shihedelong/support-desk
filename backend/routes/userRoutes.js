b3672731e72a8a66d48135a06a36c7db86be9cbf
shihedelong <shihedelong@gmail.com>
2022-02-03 13:44:51 +1100

    Protect Routes and Authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const experss = require('express')
 const router = experss.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+
+const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
-
 router.post('/login', loginUser)
+router.get('/me', protect, getMe)
 
 module.exports = router
