5b614d757251c5b974db27cd1d82cf30d395a409
shihedelong <shihedelong@gmail.com>
2022-02-03 20:51:18 +1100

    Header & Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
+
 function App() {
-  return <div>My App</div>
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login
