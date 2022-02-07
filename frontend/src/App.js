0723b3e4c1919ca9cab45df6bc8b2b180b412ade
shihedelong <shihedelong@gmail.com>
2022-02-08 10:46:25 +1100

    Route Guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import NewTicket from './pages/NewTicket'
 import Header from './components/Header'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
             <Route path='/register' element={<Register />}></Route>
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />}></Route>
+            </Route>
           </Routes>
         </div>
       </Router>
