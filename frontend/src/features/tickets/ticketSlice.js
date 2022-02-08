32ab7965dd2a021d81033f3f183e4e651a1678d1
shihedelong <shihedelong@gmail.com>
2022-02-08 21:32:56 +1100

    Create Ticket Functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -11,13 +11,48 @@ const initialState = {
   message: '',
 }
 
+// creat new ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  // ticket from the form
+  // thunkAPI, this object actually has on it a method called getState, and we can get anything else from any other state we want, like user and token
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
   reducers: {
     reset: (state) => initialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(createTicket.pending, (state) => {
+      state.isLoaidng = true
+    })
+    builder.addCase(createTicket.fulfilled, (state) => {
+      state.isLoaidng = false
+      state.isSuccess = true
+    })
+    builder.addCase(createTicket.rejected, (state, action) => {
+      state.isLoaidng = false
+      state.isError = true
+      state.message = action.payload
+    })
+  },
 })
 
 export const { reset } = ticketSlice.actions
